bb1750ee9eb9b4077852e2099056431c0c08ca10
Alex Krantz <alex@alexkrantz.com>
2018-12-21 16:42:28 -0800

    Add project creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,80 @@
 import React from 'react';
 
 import './App.css';
-import {Button, Navbar} from "@blueprintjs/core";
-import {Alignment as ALIGNMENT} from "@blueprintjs/core";
+import {Button, Navbar, Alignment, Toaster} from "@blueprintjs/core";
 import history from './history';
+import AddProject from './components/AddProject';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            add: false
+        }
+    }
+
     login() {
-    this.props.auth.login();
-  }
+        this.props.auth.login();
+    }
 
-  logout() {
-    this.props.auth.logout();
-  }
+    logout() {
+        this.props.auth.logout();
+    }
 
-  render() {
-    const { isAuthenticated } = this.props.auth;
+    onHome() {
+        history.replace("/home");
+    }
 
-    if (this.props.location.pathname === "/") history.replace("/home");
+    openAdd() {
+        if (this.props.auth.isAuthenticated()) this.setState({add: !this.state.add});
+        else {
+            let t = Toaster.create({canEscapeKeyClear: true, autoFocus: true});
+            t.show({
+                message: "You must be logged in to request a project.",
+                intent: "warning",
+                icon: "warning-sign",
+                action: {
+                    onClick: () => {this.login()},
+                    text: "Login"
+                }
+            });
+        }
+    }
 
-    return (
-        <div>
-            <Navbar>
-                <Navbar.Group align={ALIGNMENT.LEFT}>
-                    <Navbar.Heading>Projects Storage</Navbar.Heading>
-                    <Navbar.Divider/>
-                    <Button className="bp3-minimal" icon="home" text="Home" />
-                </Navbar.Group>
+    closeAdd() {
+        this.setState({add: false});
+    }
 
-                <Navbar.Group align={ALIGNMENT.RIGHT}>
-                    { !isAuthenticated() && <Button className="bp3-minimal" icon="user" text="Login" intent="primary" onClick={this.login.bind(this)}/> }
-                    { isAuthenticated() && <Button className="bp3-minimal" icon="user" text="Logout" intent="danger" onClick={this.logout.bind(this)}/> }
-                </Navbar.Group>
-            </Navbar>
-        </div>
-    );
-  }
+    createProject(data) {
+        // TODO: send request to create project
+        window.location.reload();
+    }
+
+    render() {
+        const { isAuthenticated } = this.props.auth;
+
+        if (this.props.location.pathname === "/") history.replace("/home");
+
+        return (
+            <div>
+                <Navbar>
+                    <Navbar.Group align={Alignment.LEFT}>
+                        <Navbar.Heading>Projects Storage</Navbar.Heading>
+                        <Navbar.Divider/>
+                        <Button className="bp3-minimal" icon="home" text="Home" onClick={this.onHome}/>
+                        <Button className="bp3-minimal" icon="add" text="New Project" onClick={this.openAdd.bind(this)}/>
+                    </Navbar.Group>
+
+                    <Navbar.Group align={Alignment.RIGHT}>
+                        { !isAuthenticated() && <Button className="bp3-minimal" icon="user" text="Login" intent="primary" onClick={this.login.bind(this)}/> }
+                        { isAuthenticated() && <Button className="bp3-minimal" icon="user" text="Logout" intent="danger" onClick={this.logout.bind(this)}/> }
+                    </Navbar.Group>
+                </Navbar>
+                <AddProject open={this.state.add} close={this.closeAdd.bind(this)} create={this.createProject.bind(this)}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ frontend/src/components/AddProject.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {Button, Dialog, Classes, FormGroup, InputGroup, TextArea} from "@blueprintjs/core";
+
+class AddProject extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            description: "",
+            profile: JSON.parse(localStorage.getItem("profile")).name
+        }
+    }
+
+    handleUpdate(event) {
+        switch (event.target.id) {
+            case "title":
+                this.setState({title: event.target.value});
+                break;
+
+            case "description":
+                this.setState({description: event.target.value});
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    submit() {
+        this.props.create({
+            ...this.state,
+            status: "queued",
+            added_date: new Date().toDateString(),
+            status_date: new Date().toDateString(),
+            edited_date: false
+        });
+    }
+
+    render() {
+        return (
+            <Dialog icon="add" title="Add Project" isOpen={this.props.open} onClose={this.props.close}>
+                <div className={Classes.DIALOG_BODY}>
+                    <FormGroup label="Project Title" labelFor="title">
+                        <InputGroup id="title" placeholder="Title..." value={this.state.title} onChange={this.handleUpdate.bind(this)}/>
+                    </FormGroup>
+                    <FormGroup label="Project Description" labelFor="description">
+                        <TextArea id="description" className="bp3-fill" value={this.state.description} placeholder="Description..." onChange={this.handleUpdate.bind(this)}/>
+                    </FormGroup>
+                    <FormGroup label="Author" labelFor="author">
+                        <InputGroup id="author" value={this.state.profile} disabled={true}/>
+                    </FormGroup>
+                </div>
+                <div className={Classes.DIALOG_FOOTER}>
+                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                        <Button onClick={this.props.close} minimal={true} text="Cancel"/>
+                        <Button onClick={this.submit.bind(this)} intent="success" text="Create"/>
+                    </div>
+                </div>
+            </Dialog>
+        )
+    }
+}
+
+export default AddProject;
