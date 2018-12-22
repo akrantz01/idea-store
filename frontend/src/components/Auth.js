77b7c0db2a33ff8ce8408dd28457cea341c0849c
Alex Krantz <alex@alexkrantz.com>
2018-12-21 16:25:27 -0800

    Fix profile not being removed on logout

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -33,6 +33,7 @@ export default class Auth {
         localStorage.removeItem("access_token");
         localStorage.removeItem("id_token");
         localStorage.removeItem("expires_at");
+        localStorage.removeItem("profile");
 
         history.replace("/home");
     }
